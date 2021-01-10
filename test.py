62f356e5f39ceab9475381d647b9780b5386d44d
Subramanya <subbupkb@gamail.com>
2021-01-10 22:51:33 +0530

    Inital commit

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+###################################################################
+#
+
+###################################################################
+##-----------------------  IMPORT section ----------------------###
+###################################################################
+import sys, os, time
+import subprocess
+import logging
+from os import path
+from datetime import datetime, timedelta
+import argparse
+import datetime
+
+
+
+def main():
+	print("Subbu")
+
+
+if __name__ == "__main__" : main();
