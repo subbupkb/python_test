2158925326cac5d5f50554c6e16ef5c61886fcc5
Subramanya <subbupkb@gamail.com>
2021-01-14 00:24:03 +0530

    update

--- test.py
+++ test.py
@@ -16,10 +16,17 @@ import datetime
 from kivy.app import App
 from kivy.uix.button import Button
 
+###################################################################
+
 
 class langLearnApp(App):
 	def build(self):
 		return Button(text = "Hello Subbu")
 
+
+###################################################################
+
 if __name__ == "__main__" :
 	langLearnApp().run()
+
+###################################################################
